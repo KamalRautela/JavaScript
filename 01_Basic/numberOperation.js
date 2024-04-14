a1d99daf10d7466b857e39c1f2b3bc7b8330cd71
KAMAL SINGH RAUTELA <125349042+KamalRautela@users.noreply.github.com>
2024-04-14 17:05:15 +0000

    numberOperation

--- 01_Basic/numberOperation.js
+++ 01_Basic/numberOperation.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+/* let score = "33abc"
 
 // console.log(typeof score)
 
@@ -23,4 +23,30 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(stringNumber)
-console.log(typeof stringNumber)
+console.log(typeof stringNumber) */
+
+// ***********************Operation***************************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)   // generates negative value
+
+let str1 = "Kamal"
+let str2 = " Rautela"
+let str3 = str1 + str2
+/* console.log(str3)
+
+console.log("1" + 2)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"") */
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
